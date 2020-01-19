dd93b55fbe843f02cc16556771a0fc9a7be3cfbe
Sisi <dqgsh1055@gmail.com>
2020-01-19 18:22:38 +0900

    Initial commit

---
+++ src/main/java/com/example/ubicshoppingmall/UbicshoppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.example.ubicshoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbicshoppingmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbicshoppingmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ubicshoppingmall/UbicshoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ubicshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbicshoppingmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
